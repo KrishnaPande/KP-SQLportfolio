a76a10f936edf16591f4397860e035ed631ad688
Krishna Pande <95022153+KrishnaPande@users.noreply.github.com>
2025-03-24 00:37:29 +0530

    pandas_py file

---
+++ my_pandas_script2.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('DS_bike_sales_india.csv')  # Read CSV file into a DataFrame
+print(df.head(10))  # Display the DataFrame
+import os
+print(os.getcwd())
+
+
+
